e94170d1a0cd11bcb1b79fc51a90c47965769858
Michelle Sun <michelle.sun032@icloud.com>
2022-03-07 13:56:09 +1100

    Added alternate solution 21.

--- algorithms/c++/21-merge-two-sorted-lists.cpp
+++ algorithms/c++/21-merge-two-sorted-lists.cpp
@@ -29,6 +29,32 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* front = new ListNode();
+        ListNode* curr = front;
+        
+        while(list1 && list2) {
+            if (list1->val <= list2->val) {
+                curr->next = list1;
+                list1 = list1->next;
+            } else {
+                curr->next = list2;
+                list2 = list2->next;
+            }
+            curr = curr->next;
+        }
+        
+        if (list1)
+            curr->next = list1;
+        if (list2)
+            curr->next = list2;
+        
+        return front->next;
+    }
+};
+
 int main() {
     
     return 0;
